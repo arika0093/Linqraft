b17f6d6f14054f3373093cb961a3a143133a25b1
Arika Ishinami <delete0093@gmail.com>
2025-11-20 03:48:48 +0000

    feat: add GetOrdersAnonymousAsync endpoint to retrieve orders without customer details

--- examples/Linqraft.ApiSample/OrderController.cs
+++ examples/Linqraft.ApiSample/OrderController.cs
@@ -28,4 +28,27 @@ public partial class OrderController : ControllerBase
             })
             .ToList();
     }
+
+    [HttpGet]
+    [Route("get-orders/anonymous")]
+    public IActionResult GetOrdersAnonymousAsync()
+    {
+        var results = SampleData
+            .GetOrdersFromOtherSource()
+            .AsQueryable()
+            .SelectExpr(s => new
+            {
+                Id = s.Id,
+                CustomerName = s.Customer?.Name,
+                CustomerCountry = s.Customer?.Address?.Country?.Name,
+                CustomerCity = s.Customer?.Address?.City?.Name,
+                Items = s.OrderItems.Select(oi => new
+                {
+                    ProductName = oi.Product?.Name,
+                    Quantity = oi.Quantity,
+                }),
+            })
+            .ToList();
+        return Ok(results);
+    }
 }
