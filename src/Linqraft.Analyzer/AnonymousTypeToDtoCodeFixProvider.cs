d52d1b9a6eb1689fbe618e8a22a2a804dae7a332
Arika Ishinami <delete0093@gmail.com>
2025-11-21 00:45:19 +0900

    typo fix

--- src/Linqraft.Analyzer/AnonymousTypeToDtoCodeFixProvider.cs
+++ src/Linqraft.Analyzer/AnonymousTypeToDtoCodeFixProvider.cs
@@ -141,7 +141,7 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
                 var newNamespaceDecl = updatedNamespaceDecl.AddMembers(dtoClass);
                 newRoot = newRoot
                     .ReplaceNode(updatedNamespaceDecl, newNamespaceDecl)
-                    .NormalizeWhitespace(eol: '\n');
+                    .NormalizeWhitespace(eol: "\n");
             }
         }
         else
@@ -150,7 +150,7 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
             var dtoClass = SyntaxFactory.ParseMemberDeclaration(dtoClassCode);
             if (dtoClass != null && newRoot is CompilationUnitSyntax compilationUnit)
             {
-                newRoot = compilationUnit.AddMembers(dtoClass).NormalizeWhitespace(eol: '\n');
+                newRoot = compilationUnit.AddMembers(dtoClass).NormalizeWhitespace(eol: "\n");
             }
         }
 
