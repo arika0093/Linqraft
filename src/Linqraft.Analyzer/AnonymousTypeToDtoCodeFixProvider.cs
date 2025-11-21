d3cce1b62745387305572f139cc3c53b4d8b2a82
Arika Ishinami <delete0093@gmail.com>
2025-11-21 20:16:03 +0900

    feat: enhance DTO generation for nested anonymous types and improve test coverage

--- src/Linqraft.Analyzer/AnonymousTypeToDtoCodeFixProvider.cs
+++ src/Linqraft.Analyzer/AnonymousTypeToDtoCodeFixProvider.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Linqraft.Core;
@@ -106,53 +107,68 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
         var namespaceDecl = root.DescendantNodes()
             .OfType<BaseNamespaceDeclarationSyntax>()
             .FirstOrDefault();
-        var namespaceName = namespaceDecl?.Name.ToString() ?? "Generated";
+        var namespaceName = namespaceDecl?.Name.ToString() ?? "";
 
-        // Create DTO class info
+        // Collect all DTO classes (main + nested)
+        var allDtoClasses = new List<GenerateDtoClassInfo>();
+        var nestedDtoClasses = CollectNestedDtoClasses(dtoStructure, namespaceName);
+        allDtoClasses.AddRange(nestedDtoClasses);
+
+        // Create main DTO class info
         var dtoClassInfo = new GenerateDtoClassInfo
         {
             Structure = dtoStructure,
             Accessibility = "public",
             ClassName = dtoClassName,
             Namespace = namespaceName,
-            NestedClasses = ImmutableList<GenerateDtoClassInfo>.Empty,
+            NestedClasses = [.. nestedDtoClasses],
         };
+        allDtoClasses.Add(dtoClassInfo);
 
         // Generate configuration
         var configuration = new LinqraftConfiguration();
 
-        // Build DTO class code (without namespace wrapper)
-        var dtoClassCode = dtoClassInfo.BuildCode(configuration);
-
         // Replace anonymous object with DTO instantiation
-        var newRoot = ReplaceAnonymousWithDtoSync(root, anonymousObject, dtoClassName);
+        var newRoot = ReplaceAnonymousWithDtoSync(
+            root,
+            anonymousObject,
+            dtoClassName,
+            namespaceName,
+            semanticModel
+        );
 
-        // Add DTO class to the end of the file
-        if (namespaceDecl != null)
+        // Add all DTO classes to the file (nested first, then main)
+        foreach (var dtoClass in allDtoClasses)
         {
-            // Add inside the namespace - get the updated namespace from newRoot
-            var dtoClass = SyntaxFactory.ParseMemberDeclaration(dtoClassCode);
-            if (dtoClass != null)
+            var dtoClassCode = dtoClass.BuildCode(configuration);
+
+            if (namespaceDecl != null)
             {
-                var updatedNamespaceDecl = newRoot
-                    .DescendantNodes()
-                    .OfType<BaseNamespaceDeclarationSyntax>()
-                    .First();
-                var newNamespaceDecl = updatedNamespaceDecl.AddMembers(dtoClass);
-                newRoot = newRoot
-                    .ReplaceNode(updatedNamespaceDecl, newNamespaceDecl)
-                    .NormalizeWhitespace(eol: "\n");
+                // Add inside the namespace - get the updated namespace from newRoot
+                var dtoMember = SyntaxFactory.ParseMemberDeclaration(dtoClassCode);
+                if (dtoMember != null)
+                {
+                    var updatedNamespaceDecl = newRoot
+                        .DescendantNodes()
+                        .OfType<BaseNamespaceDeclarationSyntax>()
+                        .First();
+                    var newNamespaceDecl = updatedNamespaceDecl.AddMembers(dtoMember);
+                    newRoot = newRoot.ReplaceNode(updatedNamespaceDecl, newNamespaceDecl);
+                }
+            }
+            else
+            {
+                // Add at file level (global namespace)
+                var dtoMember = SyntaxFactory.ParseMemberDeclaration(dtoClassCode);
+                if (dtoMember != null && newRoot is CompilationUnitSyntax compilationUnit)
+                {
+                    newRoot = compilationUnit.AddMembers(dtoMember);
+                }
             }
         }
-        else
-        {
-            // Add at file level
-            var dtoClass = SyntaxFactory.ParseMemberDeclaration(dtoClassCode);
-            if (dtoClass != null && newRoot is CompilationUnitSyntax compilationUnit)
-            {
-                newRoot = compilationUnit.AddMembers(dtoClass).NormalizeWhitespace(eol: "\n");
-            }
-        }
+
+        // Normalize whitespace at the end
+        newRoot = newRoot.NormalizeWhitespace(eol: "\n");
 
         return document.WithSyntaxRoot(newRoot);
     }
@@ -193,7 +209,10 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
             ?.DescendantNodes()
             .OfType<BaseNamespaceDeclarationSyntax>()
             .FirstOrDefault();
-        var namespaceName = namespaceDecl?.Name.ToString() ?? "Generated";
+        var namespaceName = namespaceDecl?.Name.ToString() ?? "";
+
+        // Collect all DTO classes (main + nested)
+        var nestedDtoClasses = CollectNestedDtoClasses(dtoStructure, namespaceName);
 
         // Create DTO class info
         var dtoClassInfo = new GenerateDtoClassInfo
@@ -202,14 +221,14 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
             Accessibility = "public",
             ClassName = dtoClassName,
             Namespace = namespaceName,
-            NestedClasses = ImmutableList<GenerateDtoClassInfo>.Empty,
+            NestedClasses = [.. nestedDtoClasses],
         };
 
         // Generate configuration
         var configuration = new LinqraftConfiguration();
 
         // Build DTO code
-        var dtoCode = BuildDtoFile(dtoClassInfo, configuration);
+        var dtoCode = BuildDtoFile(dtoClassInfo, nestedDtoClasses, configuration);
 
         // Create new document for the DTO
         var project = document.Project;
@@ -251,7 +270,9 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
     private static SyntaxNode ReplaceAnonymousWithDtoSync(
         SyntaxNode root,
         AnonymousObjectCreationExpressionSyntax anonymousObject,
-        string dtoClassName
+        string dtoClassName,
+        string namespaceName,
+        SemanticModel semanticModel
     )
     {
         // Build the new object creation expression
@@ -272,10 +293,17 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
                 valueExpression = init.Expression;
             }
 
+            // Check if this property has a nested anonymous object
+            var replacedExpression = ReplaceNestedAnonymousObjects(
+                valueExpression,
+                namespaceName,
+                semanticModel
+            );
+
             var assignment = SyntaxFactory.AssignmentExpression(
                 SyntaxKind.SimpleAssignmentExpression,
                 SyntaxFactory.IdentifierName(propertyName),
-                valueExpression
+                replacedExpression
             );
             initializers.Add(assignment);
         }
@@ -287,9 +315,193 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
                     SyntaxKind.ObjectInitializerExpression,
                     SyntaxFactory.SeparatedList(initializers)
                 )
-            );
+            )
+            .WithLeadingTrivia(anonymousObject.GetLeadingTrivia())
+            .WithTrailingTrivia(anonymousObject.GetTrailingTrivia());
 
         return root.ReplaceNode(anonymousObject, newObjectCreation);
+    }
+
+    /// <summary>
+    /// Recursively replaces nested anonymous objects with DTO instantiations
+    /// </summary>
+    private static ExpressionSyntax ReplaceNestedAnonymousObjects(
+        ExpressionSyntax expression,
+        string namespaceName,
+        SemanticModel semanticModel
+    )
+    {
+        // Direct anonymous object creation
+        if (expression is AnonymousObjectCreationExpressionSyntax nestedAnonymous)
+        {
+            var typeInfo = semanticModel.GetTypeInfo(nestedAnonymous);
+            var anonymousType = typeInfo.Type;
+            if (anonymousType != null)
+            {
+                // Infer source type from member accesses (same logic as DtoProperty.AnalyzeExpression)
+                ITypeSymbol? sourceType = null;
+                foreach (var initializer in nestedAnonymous.Initializers)
+                {
+                    var initExpr = initializer.Expression;
+                    if (initExpr is MemberAccessExpressionSyntax memberAccess)
+                    {
+                        var baseTypeInfo = semanticModel.GetTypeInfo(memberAccess.Expression);
+                        if (baseTypeInfo.Type is not null)
+                        {
+                            sourceType = baseTypeInfo.Type;
+                            break;
+                        }
+                    }
+                }
+                sourceType ??= anonymousType;
+
+                var structure = DtoStructure.AnalyzeAnonymousType(
+                    nestedAnonymous,
+                    semanticModel,
+                    sourceType
+                );
+                if (structure != null)
+                {
+                    var nestedClassName =
+                        $"{structure.SourceTypeName}Dto_{structure.GetUniqueId()}";
+
+                    // Build nested object creation
+                    var nestedInitializers = new List<ExpressionSyntax>();
+                    foreach (var init in nestedAnonymous.Initializers)
+                    {
+                        string propertyName;
+                        ExpressionSyntax valueExpression;
+
+                        if (init.NameEquals != null)
+                        {
+                            propertyName = init.NameEquals.Name.Identifier.Text;
+                            valueExpression = init.Expression;
+                        }
+                        else
+                        {
+                            propertyName = GetPropertyNameFromExpression(init.Expression);
+                            valueExpression = init.Expression;
+                        }
+
+                        // Recursively process nested expressions
+                        var replacedExpression = ReplaceNestedAnonymousObjects(
+                            valueExpression,
+                            namespaceName,
+                            semanticModel
+                        );
+
+                        var assignment = SyntaxFactory.AssignmentExpression(
+                            SyntaxKind.SimpleAssignmentExpression,
+                            SyntaxFactory.IdentifierName(propertyName),
+                            replacedExpression
+                        );
+                        nestedInitializers.Add(assignment);
+                    }
+
+                    return SyntaxFactory
+                        .ObjectCreationExpression(SyntaxFactory.IdentifierName(nestedClassName))
+                        .WithInitializer(
+                            SyntaxFactory.InitializerExpression(
+                                SyntaxKind.ObjectInitializerExpression,
+                                SyntaxFactory.SeparatedList(nestedInitializers)
+                            )
+                        )
+                        .WithLeadingTrivia(nestedAnonymous.GetLeadingTrivia())
+                        .WithTrailingTrivia(nestedAnonymous.GetTrailingTrivia());
+                }
+            }
+        }
+
+        // For lambda expressions with Select, we need to replace nested anonymous objects
+        if (
+            expression is InvocationExpressionSyntax invocation
+            && invocation.Expression is MemberAccessExpressionSyntax selectMemberAccess
+            && selectMemberAccess.Name.Identifier.Text == "Select"
+        )
+        {
+            // Get the lambda argument
+            if (
+                invocation.ArgumentList.Arguments.Count > 0
+                && invocation.ArgumentList.Arguments[0].Expression is LambdaExpressionSyntax lambda
+            )
+            {
+                // Check if lambda body is an anonymous object
+                if (lambda.Body is AnonymousObjectCreationExpressionSyntax lambdaAnonymous)
+                {
+                    // Replace the lambda body
+                    var replacedBody = ReplaceNestedAnonymousObjects(
+                        lambdaAnonymous,
+                        namespaceName,
+                        semanticModel
+                    );
+
+                    // Create new lambda with replaced body
+                    LambdaExpressionSyntax newLambda;
+                    if (lambda is SimpleLambdaExpressionSyntax simpleLambda)
+                    {
+                        newLambda = simpleLambda.WithBody(replacedBody);
+                    }
+                    else if (lambda is ParenthesizedLambdaExpressionSyntax parenLambda)
+                    {
+                        newLambda = parenLambda.WithBody(replacedBody);
+                    }
+                    else
+                    {
+                        // Fallback: return expression unchanged
+                        return expression;
+                    }
+
+                    // Replace the lambda in the invocation
+                    var newArg = invocation.ArgumentList.Arguments[0].WithExpression(newLambda);
+                    var newArgList = invocation.ArgumentList.WithArguments(
+                        invocation.ArgumentList.Arguments.Replace(
+                            invocation.ArgumentList.Arguments[0],
+                            newArg
+                        )
+                    );
+                    return invocation.WithArgumentList(newArgList);
+                }
+            }
+        }
+
+        // No replacement needed, return original expression
+        return expression;
+    }
+
+    /// <summary>
+    /// Recursively collects nested DTO classes from a structure
+    /// </summary>
+    private static List<GenerateDtoClassInfo> CollectNestedDtoClasses(
+        DtoStructure structure,
+        string namespaceName
+    )
+    {
+        var result = new List<GenerateDtoClassInfo>();
+
+        foreach (var prop in structure.Properties)
+        {
+            if (prop.NestedStructure is not null)
+            {
+                // Recursively collect nested DTOs
+                var nestedClasses = CollectNestedDtoClasses(prop.NestedStructure, namespaceName);
+                result.AddRange(nestedClasses);
+
+                // Create DTO for this nested structure
+                var nestedClassName =
+                    $"{prop.NestedStructure.SourceTypeName}Dto_{prop.NestedStructure.GetUniqueId()}";
+                var nestedDtoInfo = new GenerateDtoClassInfo
+                {
+                    Accessibility = "public",
+                    Namespace = namespaceName,
+                    ClassName = nestedClassName,
+                    Structure = prop.NestedStructure,
+                    NestedClasses = [.. nestedClasses],
+                };
+                result.Add(nestedDtoInfo);
+            }
+        }
+
+        return result;
     }
 
     private static string GenerateDtoClassName(
@@ -301,14 +513,29 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
 
     private static string BuildDtoFile(
         GenerateDtoClassInfo dtoClassInfo,
+        List<GenerateDtoClassInfo> nestedClasses,
         LinqraftConfiguration configuration
     )
     {
-        var code = dtoClassInfo.BuildCode(configuration);
+        var sb = new StringBuilder();
 
-        return $@"namespace {dtoClassInfo.Namespace};
+        // Only add namespace declaration if not in global namespace
+        if (!string.IsNullOrEmpty(dtoClassInfo.Namespace))
+        {
+            sb.AppendLine($"namespace {dtoClassInfo.Namespace};");
+            sb.AppendLine();
+        }
 
-{code}";
+        // Generate nested classes first
+        foreach (var nestedClass in nestedClasses)
+        {
+            sb.AppendLine(nestedClass.BuildCode(configuration));
+        }
+
+        // Generate main class
+        sb.AppendLine(dtoClassInfo.BuildCode(configuration));
+
+        return sb.ToString();
     }
 
     private static async Task<SyntaxNode?> ReplaceAnonymousWithDto(
@@ -323,48 +550,20 @@ public class AnonymousTypeToDtoCodeFixProvider : CodeFixProvider
         if (root == null)
             return null;
 
-        // Build the new object creation expression
-        var initializers = new List<ExpressionSyntax>();
-        foreach (var init in anonymousObject.Initializers)
-        {
-            string propertyName;
-            ExpressionSyntax valueExpression;
+        var semanticModel = await document
+            .GetSemanticModelAsync(cancellationToken)
+            .ConfigureAwait(false);
+        if (semanticModel == null)
+            return null;
 
-            if (init.NameEquals != null)
-            {
-                // Explicit: Name = value
-                propertyName = init.NameEquals.Name.Identifier.Text;
-                valueExpression = init.Expression;
-            }
-            else
-            {
-                // Implicit: need to extract property name from expression
-                propertyName = GetPropertyNameFromExpression(init.Expression);
-                valueExpression = init.Expression;
-            }
-
-            // Create assignment expression: PropertyName = value
-            var assignment = SyntaxFactory.AssignmentExpression(
-                SyntaxKind.SimpleAssignmentExpression,
-                SyntaxFactory.IdentifierName(propertyName),
-                valueExpression
-            );
-            initializers.Add(assignment);
-        }
-
-        // Create object creation: new DtoClassName { ... }
-        var newObjectCreation = SyntaxFactory
-            .ObjectCreationExpression(SyntaxFactory.IdentifierName(dtoClassName))
-            .WithInitializer(
-                SyntaxFactory.InitializerExpression(
-                    SyntaxKind.ObjectInitializerExpression,
-                    SyntaxFactory.SeparatedList(initializers)
-                )
-            );
-
-        // Replace the anonymous object with the new object creation
-        var newRoot = root.ReplaceNode(anonymousObject, newObjectCreation);
-        return newRoot;
+        // Use the synchronous version which handles nested anonymous objects
+        return ReplaceAnonymousWithDtoSync(
+            root,
+            anonymousObject,
+            dtoClassName,
+            namespaceName,
+            semanticModel
+        );
     }
 
     private static string GetPropertyNameFromExpression(ExpressionSyntax expression)
