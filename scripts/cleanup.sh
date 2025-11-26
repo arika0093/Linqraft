0f623a8f59187790e1d0e3f3e0d8a842f29c5148
Arika Ishinami <delete0093@gmail.com>
2025-11-26 02:51:47 +0000

    Fix cleanup script to properly quote ".generated" directory name

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,3 @@
 # Recursively deletes /bin, /obj, /.generated directories, then runs dotnet clean.
-find . -type d \( -name bin -o -name obj -o -name .generated \) -exec rm -rf {} +
+find . -type d \( -name bin -o -name obj -o -name ".generated" \) -exec rm -rf {} +
 dotnet clean
