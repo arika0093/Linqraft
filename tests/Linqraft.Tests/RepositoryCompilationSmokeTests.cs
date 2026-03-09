a19301b2a0d4ddc4a7aafb4839cb66fb45ce95d4
Arika Ishinami <delete0093@gmail.com>
2026-03-09 05:35:19 +0000

    wip

--- tests/Linqraft.Tests/RepositoryCompilationSmokeTests.cs
+++ tests/Linqraft.Tests/RepositoryCompilationSmokeTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Reflection;
+using System.Reflection; 
 using Linqraft.SourceGenerator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
